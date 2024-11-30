ef6cad1d4967d75fad9644be5dc30f888a060405
JesusBlan <jezublanco@gmail.com>
2024-11-30 22:39:11 +0100

    cleaned it a bit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,7 @@
 const reverseString = function(string) {
     let newString = ""
     for (let i = 0; i < string.length; ){
-        newString = newString + string.slice(-1)
+        newString += string.slice(-1)
         string = string.slice(0, -1)
     }
     return newString
